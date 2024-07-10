63360724170726df5a4b521df4487efd74aac343
김미소 <acc1mizzang@gmail.com>
2024-07-10 14:47:22 +0900

    mongoDB연결_2024.07.10_김미소

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
+// MongoDB 연결 문자열
+const uri = "mongodb+srv://test:1234@cluster0.dfioo8z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// MongoDB 연결 설정
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log('MongoDB connection error:', err));
+
+// Express 기본 라우팅
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+// 서버 실행
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
